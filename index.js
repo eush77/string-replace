aec4634aa86474d5d5d6b974c43efa2dfc1d5630
Eugene Sharygin <eush77@gmail.com>
2015-12-15 10:05:41 +0300

    split matches and contexts into separate arrays
    Having both in one array is hard to reason about and fragile.

--- index.js
+++ index.js
@@ -10,61 +10,67 @@ module.exports = function (string, pattern, replacer, opts, cb) {
   }
 
   /*
-   * Array containing match arrays interspersed with string contexts.
+   * Array containing match arrays.
    *
    * @example
    *   [
-   *     "begin ",
    *     ["one", 6, "begin one two end"],
-   *     " ",
-   *     ["two", 10, "begin one two end"],
-   *     " end"
+   *     ["two", 10, "begin one two end"]
    *   ]
    */
   var matches = [];
+
+  /*
+   * Array containing matched substrings and contexts interspersed.
+   *
+   * @example
+   *   ["begin ", "one", " ", "two", " end"]
+   */
+  var parts = [];
 
   // Size of consumed portion of the input string.
   var end = 0;
 
   string.replace(pattern, function (substring) {
+    matches.push([].slice.call(arguments));
+
     var offset = arguments[arguments.length - 2];
-    matches.push(string.slice(end, offset), [].slice.call(arguments));
+    parts.push(string.slice(end, offset), substring);
     end = offset + substring.length;
   });
-  matches.push(string.slice(end));
+  parts.push(string.slice(end));
 
-  process.nextTick(opts.parallel ? parallelLoop : sequentialLoop.bind(null, 1));
+  process.nextTick(opts.parallel ? parallelLoop : sequentialLoop.bind(null, 0));
 
   function parallelLoop () {
     var callback = afterAll(function (err) {
       return err
         ? cb(err)
-        : cb(null, matches.join(''));
+        : cb(null, parts.join(''));
     });
 
-    for (var i = 1; i < matches.length; i += 2) {
-      (function (i) {
-        replacer.apply(null, [callback(next)].concat(matches[i]));
+    matches.forEach(function (match, i) {
+      replacer.apply(null, [callback(next)].concat(match));
 
-        function next (err, replacement) {
-          // `err` is processed by `callback`.
-          matches[i] = replacement;
-        }
-      }(i));
-    }
+      function next (err, replacement) {
+        // `err` is processed by `callback`.
+        parts[1 + 2 * i] = replacement;
+      }
+    });
   }
 
   function sequentialLoop (i) {
     if (matches.length <= i) {
-      return cb(null, matches.join(''));
+      return cb(null, parts.join(''));
     }
 
     replacer.apply(null, [next].concat(matches[i]));
 
     function next (err, replacement) {
       if (err) return cb(err);
-      matches[i] = replacement;
-      sequentialLoop(i + 2);
+
+      parts[1 + 2 * i] = replacement;
+      sequentialLoop(i + 1);
     }
   }
 };
